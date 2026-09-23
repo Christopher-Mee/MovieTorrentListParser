1ff86e615e37d09f1b90e4e5bc171e4d0a515a61
Christopher-Mee <70412094+Christopher-Mee@users.noreply.github.com>
2026-09-23 19:57:16 -0400

    Updated .gitignore and added simple .bat file
    Added .gitignore entry for datasets folder
    Added simple .bat file to run the python script with the python venv

---
+++ MovieTorrentListParser.bat
@@ -0,0 +1,3 @@
+@echo off
+"%USERPROFILE%\Documents\Python Scripts\MovieTorrentListParser\venv\Scripts\python.exe" "%USERPROFILE%\Documents\Python Scripts\MovieTorrentListParser\ParseTorrentListToCSV.py" %*
+@pause
